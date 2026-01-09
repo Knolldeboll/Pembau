4c06b40c5e8b0d78d39f0e84061f1dc59c23a630
Knolldeboll <chrissi.ruetz@freenet.de>
2026-01-09 16:16:10 +0100

    platzierung der components mittels flex und gap pt.2

--- pembau/src/Body.tsx
+++ pembau/src/Body.tsx
@@ -12,16 +12,26 @@ const Body = () => {
 
         display: "flex",
         flexDirection: "column",
-        background: "#FFFFFF"
+        background: "#FFFFFF",
+        gap: "5vw"
       }}>
 
       {/* früher war die hor. verschiebung und der rotate über position absolute und dann top gemacht. 
       besser aber für alles nur translate verwenden, um den normalen dokumentenflow zu erhalten und vertikale
       positionierung dann über gap machen. */}
 
+      {/* Generell idealerweise so: 
+      1. Bodyinstance ist div mit flex, darin mit gap getrennte Sections/Frames. 
+          Das sorgt für ne Ordentliche Struktur generell. Verschiebung nach lr/ou kann dann in den Sections geschehen!
+      2. die Sections sind divs, in denen mehr freiheit Herrscht. 
+          da kann man von der regelmäßigen platzoierung abweichen durch: 
+              - horizontal translateX
+              - vertikal mit margin */}
+
       <div id="nicetryjpg" style={{
         width: "150vw",
-        transform: "translateX(-29%) rotate(1.44deg)"
+        transform: "translateX(-29%) rotate(1.44deg)",
+
 
       }}>
         <NiceTryJPG></NiceTryJPG>
@@ -35,13 +45,20 @@ const Body = () => {
         left: "11.69%",
         top: "10.57%"
       */}
-      <div style={{
-        width: "79vw"
+      <div id="textframeaussicht" style={{
+        width: "100vw",
+        backgroundColor: "#ccccccff",
+
       }}>
         <TextFrameAussicht></TextFrameAussicht>
       </div>
 
-
+      <div style={{
+        width: "79vw",
+        backgroundColor: "#ccccccff"
+      }}>
+        <TextFrameAussicht></TextFrameAussicht>
+      </div>
       {/*
       <div id="test">
         <BodyFrame2></BodyFrame2>
--- pembau/src/TextFrameAussicht.tsx
+++ pembau/src/TextFrameAussicht.tsx
@@ -4,11 +4,11 @@ const TextFrameAussicht = () => {
 
 
     return (
-        <div >
-            <p style={{ fontFamily: "DM Sans", fontWeight: "700", fontSize: "120%", color: "#FF6400" }}>Pembau_eine_gute_Aussicht_für_Innsbruck</p>
-            <p style={{ fontFamily: "DM Sans", fontWeight: "400" }}>Als Kunst- und Kulturplattform versteht sich der Pembau als ein Ort, der neuen Raum für kreative Entfaltung und gemeinsames Experimentieren bietet.Ziel ist es, mit dem Pembau einen lebendigen Ort der Begegnung zu erschaffen, an dem sich Kultur, Bildung, Gemeinschaft und Individuum im Zusammenspiel von alpin-urbanem Leben und naturnaher Harmonie entfalten können.</p>
+        < >
+            <h2 className="headline">PEMBAU<span className="span">_eine_gute_Aussicht</span>_FÜR_INNSBRUCK</h2>
+            <p className="paragraph">Als Kunst- und Kulturplattform versteht sich der Pembau als ein Ort, der neuen Raum für kreative Entfaltung und gemeinsames Experimentieren bietet.Ziel ist es, mit dem Pembau einen lebendigen Ort der Begegnung zu erschaffen, an dem sich Kultur, Bildung, Gemeinschaft und Individuum im Zusammenspiel von alpin-urbanem Leben und naturnaher Harmonie entfalten können.</p>
             <MehrErfahrenButton></MehrErfahrenButton>
-        </div>
+        </>
     )
 }
 export default TextFrameAussicht;
