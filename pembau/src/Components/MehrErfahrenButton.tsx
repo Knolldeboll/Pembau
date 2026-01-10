ceae03fdeb51bcd46de5141593e6c2bd187fd7f5
Knolldeboll <chrissi.ruetz@freenet.de>
2026-01-10 22:02:29 +0100

    icon hover color change

--- pembau/src/Components/MehrErfahrenButton.tsx
+++ pembau/src/Components/MehrErfahrenButton.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 const MehrErfahrenButton = () => {
 
 
     // So wies jetzt ist sind die echten maße des svg durch das div vorgegeben!
     // das svg gibt nur die linien innerhalb an.
+
+
+    const [isHovered, setIsHovered] = useState(true);
 
     return (
 
@@ -11,6 +16,10 @@ const MehrErfahrenButton = () => {
         /*   position: "absolute", 
         top: "5%"
         */
+
+
+
+
         <div style={{
 
             display: "inline-block",
@@ -20,15 +29,17 @@ const MehrErfahrenButton = () => {
 
         }}
 
+            onMouseEnter={() => setIsHovered(true)
+            }
+            onMouseLeave={() => setIsHovered(false)}
             onClick={() => { console.log("ass") }}>
 
             <svg viewBox="0 0 252 73" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M251.54 72.6994L229.576 0L0 0L0 72.6994L251.54 72.6994Z" fill="#1A1A1A" />
-                <text x="50%"
+                <text className={isHovered ? "icon-hover" : "icon"} x="50%"
                     y="50%"
                     dominantBaseline="middle"
                     textAnchor="middle"
-                    fill="white"
                     fontSize="19"
                     fontFamily="DM Sans, sans-serif"
                     fontWeight="700">MEHR_ERFAHREN</text>
