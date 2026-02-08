2f1e13fb018a380480ba0990dca193f84ff6228a
Knolldeboll <chrissi.ruetz@freenet.de>
2026-02-08 19:10:13 +0100

    Bisschen Winkel gemacht. Funktioniert soweit, aber nur bei AR von 1!

--- pembau/src/Components/CollapseIamgeFrame.tsx
+++ pembau/src/Components/CollapseIamgeFrame.tsx
@@ -8,33 +8,50 @@ export const CollapseImageFrame = ({ image }: CollapseImageFrameProps) => {
 
 
 
-    // TODO: Generiere Random Points für die Ecken! 
-    // Wende die dann für SVG und für das CSS-Polygon-Clipping an
 
 
-    // TODO: Den winkel der inneren Ecke richtig machen!
-    // der ist einerseits abhängig von den Faltpunkten, andererseits vom Aspect Ratio des bilds!
-    // 
-    // bei z.B. 20%/20% Faltung und AR von 1 wäre die Faltecke genau 90% .
-    // bei 20%20 und AR von 1/2 
 
-    // z.B. ist beim svg
+    // TODO: irgendwo das Aspect Ratio herbekommen.
+    // Da die Bilddateien vorgegeben sind und später nicht gestretched werden, 
+    // kann man auch vor der Laufzeit schon die ARs bestimmen.
+    // Wenn man später z.B. d
+
+
+    // immer zw. 5 und 25
+    // const topRightTop = Math.random() * 20 + 5  // 
+    // const topRightRight = Math.random() * 20 + 5  // 
+
+    const topRightTop = 10;
+    const topRightRight = 20;
+    // calc the angle of the hypothenusis
+
+    // TODO: Aspect Ratio mit einfließen lassen! Anfangs war das 1:1, das hat sich rausgekürzt.
+    // z.B. bei 1:1.5 also 1* a und 1.5 * b oder so verwenden! 
+
+    // denn 20% auf der kurzen seite sind ggf. weniger als 20% auf der anderen seite, auch wenns gleich aussieht.
+
+    let angle = (Math.atan(topRightRight / topRightTop) * (180 / Math.PI));
+
+    console.log("angle" + angle);
+
 
     // pos relative, damit die ecken Absolute platziert werden können!
-    return (<div className="collapseimageframe" style={{ position: "relative", width: "500px", height: "500px" }}>
 
-        <img src={image} style={{ width: "100%", height: "100%", clipPath: "polygon(60% 0%, 100% 20%, 100% 100%, 0% 100%, 0% 0%)" }}>
+    // TODO: Bei polygon noch mit ´´ Arbeiten, damit man die Werte von oben einbinden kann!
+    return (<div className="collapseimageframe" style={{ position: "relative", width: "100%", height: "100%" }}>
+
+        <img src={image} style={{ width: "100%", height: "100%", clipPath: `polygon(${100 - topRightTop}% 0%, 100% ${topRightRight}%, 100% 100%, 0% 100%, 0% 0%)` }}>
 
         </img>
 
 
         {/** jetzt Kommen noch SVG-Ecken */}
-        <svg style={{ position: "absolute", width: "40%", height: "20%", top: "0", right: "0", fill: "#386638", scale: "1 -1", rotate: "0deg" }} viewBox="0 0 100 100" preserveAspectRatio="none">
+        <svg style={{ position: "absolute", width: topRightTop + "%", height: topRightRight + "%", top: "0", right: "0", scale: "-1 1", rotate: `-${37}deg`, fill: "#386638" }} viewBox="0 0 100 100" preserveAspectRatio="none">
             <polygon points="0,0 100,0 100,100"></polygon>
         </svg>
 
 
 
-    </div>);
+    </div >);
 }
 
