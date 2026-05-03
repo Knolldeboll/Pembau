ef6bcae0d941b9d5b143797a26d8612bf65b3706
Knolldeboll <chrissi.ruetz@freenet.de>
2026-05-03 22:32:52 +0200

    lp layout fix at end

--- pembau/src/LandingPage.tsx
+++ pembau/src/LandingPage.tsx
@@ -166,7 +166,7 @@ const LandingPage = () => {
         <TextFrameEndeLP></TextFrameEndeLP>
       </div >
 
-      <div id="endbuttons" style={{ display: "flex", flexDirection: "column", gap: "1vw", marginTop: "10vw", width: "40vw", height: "30vw", transform: "translateX(10vw)" }} >
+      <div id="endbuttons" style={{ display: "flex", flexDirection: "column", gap: "1vw", marginTop: "10vh", width: "40vw", height: "auto", transform: "translateX(10vw)" }} >
         <TextButton text="ABOUT" onClick={() => { navigate("/Pembau/About") }}></TextButton>
         <TextButton text="INSTAGRAM" onClick={() => { console.log("TODO: Isnta ocnlick") }}></TextButton>
         <TextButton text="ZURÜCK_ZUM_ANFANG" onClick={() => { console.log("TODO: Zurück ocnlick") }}></TextButton>
